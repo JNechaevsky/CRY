754caf90908dcd369abf35e6a803f9cc63798a68
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2024-03-30 10:02:15 +0300

    Jaguar code for p_maputl

--- src/doom/p_maputl.c
+++ src/doom/p_maputl.c
@@ -34,63 +34,61 @@
 #include "id_func.h"
 
 
-//
-// P_AproxDistance
-// Gives an estimation of distance (not exact)
-//
+/*
+===================
+=
+= P_AproxDistance
+=
+= Gives an estimation of distance (not exact)
+=
+===================
+*/
 
-fixed_t
-P_AproxDistance
-( fixed_t	dx,
-  fixed_t	dy )
+fixed_t P_AproxDistance (fixed_t dx, fixed_t dy)
 {
-    dx = abs(dx);
-    dy = abs(dy);
-    if (dx < dy)
-	return dx+dy-(dx>>1);
-    return dx+dy-(dy>>1);
+	dx = abs(dx);
+	dy = abs(dy);
+	if (dx < dy)
+		return dx+dy-(dx>>1);
+	return dx+dy-(dy>>1);
 }
 
+/*
+==================
+=
+= P_PointOnLineSide
+=
+= Returns 0 or 1
+==================
+*/
 
-//
-// P_PointOnLineSide
-// Returns 0 or 1
-//
-int
-P_PointOnLineSide
-( fixed_t	x,
-  fixed_t	y,
-  line_t*	line )
+int P_PointOnLineSide (fixed_t x, fixed_t y, line_t *line)
 {
-    fixed_t	dx;
-    fixed_t	dy;
-    fixed_t	left;
-    fixed_t	right;
+	fixed_t	dx,dy;
+	fixed_t	left, right;
 	
-    if (!line->dx)
-    {
-	if (x <= line->v1->x)
-	    return line->dy > 0;
+	if (!line->dx)
+	{
+		if (x <= line->v1->x)
+			return line->dy > 0;
+		return line->dy < 0;
+	}
+	if (!line->dy)
+	{
+		if (y <= line->v1->y)
+			return line->dx < 0;
+		return line->dx > 0;
+	}
 	
-	return line->dy < 0;
-    }
-    if (!line->dy)
-    {
-	if (y <= line->v1->y)
-	    return line->dx < 0;
+	dx = (x - line->v1->x);
+	dy = (y - line->v1->y);
 	
-	return line->dx > 0;
-    }
+	left = (line->dy>>16) * (dx>>16);
+	right = (dy>>16) * (line->dx>>16);
 	
-    dx = (x - line->v1->x);
-    dy = (y - line->v1->y);
-	
-    left = FixedMul ( line->dy>>FRACBITS , dx );
-    right = FixedMul ( dy , line->dx>>FRACBITS );
-	
-    if (right < left)
-	return 0;		// front side
-    return 1;			// back side
+	if (right < left)
+		return 0;		/* front side */
+	return 1;			/* back side */
 }
 
 
@@ -146,70 +144,66 @@ P_BoxOnLineSide
     return -1;
 }
 
+/*
+==================
+=
+= P_PointOnDivlineSide
+=
+= Returns 0 or 1
+==================
+*/
 
-//
-// P_PointOnDivlineSide
-// Returns 0 or 1.
-//
-int
-P_PointOnDivlineSide
-( fixed_t	x,
-  fixed_t	y,
-  divline_t*	line )
+int P_PointOnDivlineSide (fixed_t x, fixed_t y, divline_t *line)
 {
-    fixed_t	dx;
-    fixed_t	dy;
-    fixed_t	left;
-    fixed_t	right;
+	fixed_t	dx,dy;
+	fixed_t	left, right;
 	
-    if (!line->dx)
-    {
-	if (x <= line->x)
-	    return line->dy > 0;
+	if (!line->dx)
+	{
+		if (x <= line->x)
+			return line->dy > 0;
+		return line->dy < 0;
+	}
+	if (!line->dy)
+	{
+		if (y <= line->y)
+			return line->dx < 0;
+		return line->dx > 0;
+	}
 	
-	return line->dy < 0;
-    }
-    if (!line->dy)
-    {
-	if (y <= line->y)
-	    return line->dx < 0;
-
-	return line->dx > 0;
-    }
+	dx = (x - line->x);
+	dy = (y - line->y);
 	
-    dx = (x - line->x);
-    dy = (y - line->y);
+/* try to quickly decide by looking at sign bits */
+	if ( (line->dy ^ line->dx ^ dx ^ dy)&0x80000000 )
+	{
+		if ( (line->dy ^ dx) & 0x80000000 )
+			return 1;	/* (left is negative) */
+		return 0;
+	}
 	
-    // try to quickly decide by looking at sign bits
-    if ( (line->dy ^ line->dx ^ dx ^ dy)&0x80000000 )
-    {
-	if ( (line->dy ^ dx) & 0x80000000 )
-	    return 1;		// (left is negative)
-	return 0;
-    }
+	left = FixedMul ( line->dy>>8, dx>>8 );
+	right = FixedMul ( dy>>8 , line->dx>>8 );
 	
-    left = FixedMul ( line->dy>>8, dx>>8 );
-    right = FixedMul ( dy>>8 , line->dx>>8 );
-	
-    if (right < left)
-	return 0;		// front side
-    return 1;			// back side
+	if (right < left)
+		return 0;		/* front side */
+	return 1;			/* back side */
 }
 
+/*
+==============
+=
+= P_MakeDivline
+=
+==============
+*/
 
-
-//
-// P_MakeDivline
-//
-void
-P_MakeDivline
-( line_t*	li,
-  divline_t*	dl )
+void P_MakeDivline (line_t *li, divline_t *dl)
 {
-    dl->x = li->v1->x;
-    dl->y = li->v1->y;
-    dl->dx = li->dx;
-    dl->dy = li->dy;
+	dl->x = li->v1->x;
+	dl->y = li->v1->y;
+	dl->dx = li->dx;
+	dl->dy = li->dy;
 }
 
 
@@ -226,7 +220,6 @@ P_InterceptVector
 ( divline_t*	v2,
   divline_t*	v1 )
 {
-#if 1
     fixed_t	frac;
     fixed_t	num;
     fixed_t	den;
@@ -244,290 +237,225 @@ P_InterceptVector
     frac = FixedDiv (num , den);
 
     return frac;
-#else	// UNUSED, float debug.
-    float	frac;
-    float	num;
-    float	den;
-    float	v1x;
-    float	v1y;
-    float	v1dx;
-    float	v1dy;
-    float	v2x;
-    float	v2y;
-    float	v2dx;
-    float	v2dy;
-
-    v1x = (float)v1->x/FRACUNIT;
-    v1y = (float)v1->y/FRACUNIT;
-    v1dx = (float)v1->dx/FRACUNIT;
-    v1dy = (float)v1->dy/FRACUNIT;
-    v2x = (float)v2->x/FRACUNIT;
-    v2y = (float)v2->y/FRACUNIT;
-    v2dx = (float)v2->dx/FRACUNIT;
-    v2dy = (float)v2->dy/FRACUNIT;
-	
-    den = v1dy*v2dx - v1dx*v2dy;
-
-    if (den == 0)
-	return 0;	// parallel
-    
-    num = (v1x - v2x)*v1dy + (v2y - v1y)*v1dx;
-    frac = num / den;
-
-    return frac*FRACUNIT;
-#endif
 }
 
+/*
+==================
+=
+= P_LineOpening
+=
+= Sets opentop and openbottom to the window through a two sided line
+= OPTIMIZE: keep this precalculated
+==================
+*/
 
-//
-// P_LineOpening
-// Sets opentop and openbottom to the window
-// through a two sided line.
-// OPTIMIZE: keep this precalculated
-//
-fixed_t opentop;
-fixed_t openbottom;
-fixed_t openrange;
+fixed_t opentop, openbottom, openrange;
 fixed_t	lowfloor;
 
 
 void P_LineOpening (line_t* linedef)
 {
-    sector_t*	front;
-    sector_t*	back;
+	sector_t	*front, *back;
 	
-    if (linedef->sidenum[1] == NO_INDEX) // [crispy] extended nodes
-    {
-	// single sided line
-	openrange = 0;
-	return;
-    }
+	if (linedef->sidenum[1] == NO_INDEX) // [crispy] extended nodes
+	{	/* single sided line */
+		openrange = 0;
+		return;
+	}
 	 
-    front = linedef->frontsector;
-    back = linedef->backsector;
+	front = linedef->frontsector;
+	back = linedef->backsector;
 	
-    if (front->ceilingheight < back->ceilingheight)
-	opentop = front->ceilingheight;
-    else
-	opentop = back->ceilingheight;
-
-    if (front->floorheight > back->floorheight)
-    {
-	openbottom = front->floorheight;
-	lowfloor = back->floorheight;
-    }
-    else
-    {
-	openbottom = back->floorheight;
-	lowfloor = front->floorheight;
-    }
-	
-    openrange = opentop - openbottom;
-}
-
-
-//
-// THING POSITION SETTING
-//
-
-
-//
-// P_UnsetThingPosition
-// Unlinks a thing from block map and sectors.
-// On each position change, BLOCKMAP and other
-// lookups maintaining lists ot things inside
-// these structures need to be updated.
-//
-void P_UnsetThingPosition (mobj_t* thing)
-{
-    int		blockx;
-    int		blocky;
-
-    if ( ! (thing->flags & MF_NOSECTOR) )
-    {
-	// inert things don't need to be in blockmap?
-	// unlink from subsector
-	if (thing->snext)
-	    thing->snext->sprev = thing->sprev;
-
-	if (thing->sprev)
-	    thing->sprev->snext = thing->snext;
+	if (front->ceilingheight < back->ceilingheight)
+		opentop = front->ceilingheight;
 	else
-	    thing->subsector->sector->thinglist = thing->snext;
-    }
-	
-    if ( ! (thing->flags & MF_NOBLOCKMAP) )
-    {
-	// inert things don't need to be in blockmap
-	// unlink from block map
-	if (thing->bnext)
-	    thing->bnext->bprev = thing->bprev;
-	
-	if (thing->bprev)
-	    thing->bprev->bnext = thing->bnext;
-	else
+		opentop = back->ceilingheight;
+	if (front->floorheight > back->floorheight)
 	{
-	    blockx = (thing->x - bmaporgx)>>MAPBLOCKSHIFT;
-	    blocky = (thing->y - bmaporgy)>>MAPBLOCKSHIFT;
-
-	    if (blockx>=0 && blockx < bmapwidth
-		&& blocky>=0 && blocky <bmapheight)
-	    {
-		blocklinks[blocky*bmapwidth+blockx] = thing->bnext;
-	    }
-	}
-    }
-}
-
-
-//
-// P_SetThingPosition
-// Links a thing into both a block and a subsector
-// based on it's x y.
-// Sets thing->subsector properly
-//
-void
-P_SetThingPosition (mobj_t* thing)
-{
-    subsector_t*	ss;
-    sector_t*		sec;
-    int			blockx;
-    int			blocky;
-    mobj_t**		link;
-
-    
-    // link into subsector
-    ss = R_PointInSubsector (thing->x,thing->y);
-    thing->subsector = ss;
-    
-    if ( ! (thing->flags & MF_NOSECTOR) )
-    {
-	// invisible things don't go into the sector links
-	sec = ss->sector;
-	
-	thing->sprev = NULL;
-	thing->snext = sec->thinglist;
-
-	if (sec->thinglist)
-	    sec->thinglist->sprev = thing;
-
-	sec->thinglist = thing;
-    }
-
-    
-    // link into blockmap
-    if ( ! (thing->flags & MF_NOBLOCKMAP) )
-    {
-	// inert things don't need to be in blockmap		
-	blockx = (thing->x - bmaporgx)>>MAPBLOCKSHIFT;
-	blocky = (thing->y - bmaporgy)>>MAPBLOCKSHIFT;
-
-	if (blockx>=0
-	    && blockx < bmapwidth
-	    && blocky>=0
-	    && blocky < bmapheight)
-	{
-	    link = &blocklinks[blocky*bmapwidth+blockx];
-	    thing->bprev = NULL;
-	    thing->bnext = *link;
-	    if (*link)
-		(*link)->bprev = thing;
-
-	    *link = thing;
+		openbottom = front->floorheight;
+		lowfloor = back->floorheight;
 	}
 	else
 	{
-	    // thing is off the map
-	    thing->bnext = thing->bprev = NULL;
+		openbottom = back->floorheight;
+		lowfloor = front->floorheight;
 	}
-    }
+	
+	openrange = opentop - openbottom;
 }
 
+/*
+===============================================================================
 
+						THING POSITION SETTING
 
-//
-// BLOCK MAP ITERATORS
-// For each line/thing in the given mapblock,
-// call the passed PIT_* function.
-// If the function returns false,
-// exit with false without checking anything else.
-//
+===============================================================================
+*/
 
+/*
+===================
+=
+= P_UnsetThingPosition 
+=
+= Unlinks a thing from block map and sectors
+=
+===================
+*/
 
-//
-// P_BlockLinesIterator
-// The validcount flags are used to avoid checking lines
-// that are marked in multiple mapblocks,
-// so increment validcount before the first call
-// to P_BlockLinesIterator, then make one or more calls
-// to it.
-//
-boolean
-P_BlockLinesIterator
-( int			x,
-  int			y,
-  boolean(*func)(line_t*) )
+void P_UnsetThingPosition (mobj_t *thing)
 {
-    int			offset;
-    int32_t*		list; // [crispy] BLOCKMAP limit
-    line_t*		ld;
+	int				blockx, blocky;
+
+	if ( ! (thing->flags & MF_NOSECTOR) )
+	{	/* inert things don't need to be in blockmap */
+/* unlink from subsector */
+		if (thing->snext)
+			thing->snext->sprev = thing->sprev;
+		if (thing->sprev)
+			thing->sprev->snext = thing->snext;
+		else
+			thing->subsector->sector->thinglist = thing->snext;
+	}
 	
-    if (x<0
-	|| y<0
-	|| x>=bmapwidth
-	|| y>=bmapheight)
-    {
-	return true;
-    }
-    
-    offset = y*bmapwidth+x;
+	if ( ! (thing->flags & MF_NOBLOCKMAP) )
+	{	/* inert things don't need to be in blockmap */
+/* unlink from block map */
+		if (thing->bnext)
+			thing->bnext->bprev = thing->bprev;
+		if (thing->bprev)
+			thing->bprev->bnext = thing->bnext;
+		else
+		{
+			blockx = (thing->x - bmaporgx)>>MAPBLOCKSHIFT;
+			blocky = (thing->y - bmaporgy)>>MAPBLOCKSHIFT;
+			blocklinks[blocky*bmapwidth+blockx] = thing->bnext;
+		}
+	}
+}
+
+/*
+===================
+=
+= P_SetThingPosition 
+=
+= Links a thing into both a block and a subsector based on it's x y
+= Sets thing->subsector properly
+=
+===================
+*/
+
+void P_SetThingPosition (mobj_t *thing)
+{
+	subsector_t		*ss;
+	sector_t		*sec;
+	int				blockx, blocky;
+	mobj_t			**link;
 	
-    offset = *(blockmap+offset);
+/* */
+/* link into subsector */
+/* */
+	ss = R_PointInSubsector (thing->x,thing->y);
+	thing->subsector = ss;
+	if ( ! (thing->flags & MF_NOSECTOR) )
+	{	/* invisible things don't go into the sector links */
+		sec = ss->sector;
+	
+		thing->sprev = NULL;
+		thing->snext = sec->thinglist;
+		if (sec->thinglist)
+			sec->thinglist->sprev = thing;
+		sec->thinglist = thing;
+	}
+	
+/* */
+/* link into blockmap */
+/* */
+	if ( ! (thing->flags & MF_NOBLOCKMAP) )
+	{	/* inert things don't need to be in blockmap		 */
+		blockx = (thing->x - bmaporgx)>>MAPBLOCKSHIFT;
+		blocky = (thing->y - bmaporgy)>>MAPBLOCKSHIFT;
+		if (blockx>=0 && blockx < bmapwidth && blocky>=0 && blocky <bmapheight)
+		{
+			link = &blocklinks[blocky*bmapwidth+blockx];
+			thing->bprev = NULL;
+			thing->bnext = *link;
+			if (*link)
+				(*link)->bprev = thing;
+			*link = thing;
+		}
+		else
+		{	/* thing is off the map */
+			thing->bnext = thing->bprev = NULL;
+		}
+	}
+}
 
-    for ( list = blockmaplump+offset ; *list != -1 ; list++)
-    {
-	ld = &lines[*list];
+/*
+===============================================================================
 
-	if (ld->validcount == validcount)
-	    continue; 	// line has already been checked
+						BLOCK MAP ITERATORS
 
-	ld->validcount = validcount;
+For each line/thing in the given mapblock, call the passed function.
+If the function returns false, exit with false without checking anything else.
+
+===============================================================================
+*/
+
+/*
+==================
+=
+= P_BlockLinesIterator
+=
+= The validcount flags are used to avoid checking lines
+= that are marked in multiple mapblocks, so increment validcount before
+= the first call to P_BlockLinesIterator, then make one or more calls to it
+===================
+*/
+
+boolean P_BlockLinesIterator (int x, int y, boolean(*func)(line_t*) )
+{
+	int			offset;
+    int32_t		*list; // [crispy] BLOCKMAP limit
+	line_t		*ld;
+	
+	if (x<0 || y<0 || x>=bmapwidth || y>=bmapheight)
+		return true;
+	offset = y*bmapwidth+x;
+	
+	offset = *(blockmap+offset);
+
+	for ( list = blockmaplump+offset ; *list != -1 ; list++)
+	{
+		ld = &lines[*list];
+		if (ld->validcount == validcount)
+			continue;		/* line has already been checked */
+		ld->validcount = validcount;
 		
-	if ( !func(ld) )
-	    return false;
-    }
-    return true;	// everything was checked
+		if ( !func(ld) )
+			return false;
+	}
+	
+	return true;		/* everything was checked */
 }
 
+/*
+==================
+=
+= P_BlockThingsIterator
+=
+==================
+*/
 
-//
-// P_BlockThingsIterator
-//
-boolean
-P_BlockThingsIterator
-( int                   x,
-  int                   y,
-  boolean(*func)(mobj_t*) )
+boolean P_BlockThingsIterator (int x, int y, boolean(*func)(mobj_t*) )
 {
-    mobj_t*             mobj;
+	mobj_t		*mobj;
+	
+	if (x<0 || y<0 || x>=bmapwidth || y>=bmapheight)
+		return true;
 
-    if ( x<0
-         || y<0
-         || x>=bmapwidth
-         || y>=bmapheight)
-    {
-        return true;
-    }
-
-    LINKED_LIST_CHECK_NO_CYCLE(mobj_t, blocklinks[y*bmapwidth+x], bnext);
-
-    for (mobj = blocklinks[y*bmapwidth+x] ;
-         mobj ;
-         mobj = mobj->bnext)
-    {
-        if (!func( mobj ) )
-            return false;
-    }
+	for (mobj = blocklinks[y*bmapwidth+x] ; mobj ; mobj = mobj->bnext)
+		if (!func( mobj ) )
+			return false;	
 
     // [JN] Do not apply following BLOCKMAP fix for explosion radius damage.
     // Otherwise, explosion damage will be multiplied on ammount of BLOCKMAP 
