21f0511d1612f0d930b42af0c7e67ab18ba57786
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2024-07-16 23:40:33 +0300

    Monsters doesn't stuck on doortracks

--- src/doom/p_enemy.c
+++ src/doom/p_enemy.c
@@ -337,6 +337,35 @@ boolean P_Move (mobj_t*	actor)
 			
 	actor->movedir = DI_NODIR;
 	good = false;
+
+	// [JN] Jaguar: monsters doesn't stuck on doortracks.
+	// https://doomwiki.org/wiki/Monsters_stuck_in_doortracks,_walls_or_hanging_off_lifts
+	//
+	// if the special is not a door that can be opened, return false
+	//
+	// killough 8/9/98: this is what caused monsters to get stuck in
+	// doortracks, because it thought that the monster freed itself
+	// by opening a door, even if it was moving towards the doortrack,
+	// and not the door itself.
+	//
+	// killough 9/9/98: If a line blocking the monster is activated,
+	// return true 90% of the time. If a line blocking the monster is
+	// not activated, but some other line is, return false 90% of the
+	// time. A bit of randomness is needed to ensure it's free from
+	// lockups, but for most cases, it returns the correct result.
+	//
+	// Do NOT simply return false 1/4th of the time (causes monsters to
+	// back out when they shouldn't, and creates secondary stickiness).
+	{
+	    const line_t *blockline = spechit[numspechit];
+
+	    for (good = false; numspechit--;)
+	        if (P_UseSpecialLine(actor, spechit[numspechit], 0))
+	            good |= spechit[numspechit] == blockline ? 1 : 2;
+    
+	    return (good && ((P_Random() >= 230) ^ (good & 1)));
+	}
+
 	while (numspechit--)
 	{
 	    ld = spechit[numspechit];
