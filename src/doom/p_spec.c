56a6337e04f103e77d528e41e3cbcf5db8402c10
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2024-07-19 16:19:13 +0300

    No-op for unknown sector specials

--- src/doom/p_spec.c
+++ src/doom/p_spec.c
@@ -791,8 +791,8 @@ void P_PlayerInSpecialSector (player_t *player)
 			break;
 			
 		default:
-			I_Error ("P_PlayerInSpecialSector: "
-					"unknown special %i",sector->special);
+			// [JN] No-op for unknown sector specials.
+			break;
 	};
 }
 
