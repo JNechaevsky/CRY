334ea4a54a395825a26d9b95f942f840ade18da7
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2024-08-05 22:36:53 +0300

    Status bar SSG indication

--- src/doom/st_bar.c
+++ src/doom/st_bar.c
@@ -254,6 +254,12 @@ static boolean WeaponAvailable (int w)
         return false;
     }
 
+    // [JN] Jaguar: can't have SSG in Evil Unleashed levels.
+    if (w == wp_supershotgun && gamemap < 25)
+    {
+        return false;
+    }
+
     return true;
 }
 
@@ -1358,8 +1364,8 @@ void ST_Drawer (boolean force)
 
     // Pistol
     ST_DrawWeaponNumberFunc(2, 245 + wide_x, 175, plyr->weaponowned[1]);
-    // Shotgun
-    ST_DrawWeaponNumberFunc(3, 257 + wide_x, 175, plyr->weaponowned[2]);
+    // Shotgun or Super Shotgun
+    ST_DrawWeaponNumberFunc(3, 257 + wide_x, 175, plyr->weaponowned[2] || plyr->weaponowned[8]);
     // Chaingun
     ST_DrawWeaponNumberFunc(4, 269 + wide_x, 175, plyr->weaponowned[3]);
     // Rocket Launcher
