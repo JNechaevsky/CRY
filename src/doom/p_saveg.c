e9a538b92d5e3eab19f137418fe23b5db1d9d6c7
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2024-08-15 00:20:23 +0300

    Retain savegame function
    Array size!!!

--- src/doom/p_saveg.c
+++ src/doom/p_saveg.c
@@ -34,8 +34,6 @@
 #include "id_vars.h"
 
 
-#define VERSIONSNAME  "CRY 2.0"
-
 FILE *save_stream;
 int savegamelength;
 boolean savegame_error;
@@ -1408,7 +1406,7 @@ void P_WriteSaveGameHeader(char *description)
         saveg_write8(0);
 
     memset(name, 0, sizeof(name));
-    M_snprintf(name, sizeof(name), VERSIONSNAME);
+    M_snprintf(name, sizeof(name), "version 2.0");
 
     for (i=0; i<VERSIONSIZE; ++i)
         saveg_write8(name[i]);
